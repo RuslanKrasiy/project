2427ee4e7fb6d9fd86c223ffb5c1a61997b2cf74
RuslanKrasiy <krasiyruslan@gmail.com>
2019-04-16 20:51:29 +0200

    install semantic-ui

--- src/index.php
+++ src/index.php
@@ -1,3 +1,15 @@
+<!Doctype html>
+<html lang="Es-es">
+<head>
+<title>Centro de Estetica</title>
+<meta charset="utf-8">
+</head>
+<body>
+
 <?php
-phpinfo();
+//phpinfo();
+
+echo "<h1> Prueba de mi proyecto1</h1>";
 ?>
+</body>
+</html>
