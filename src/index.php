8f8ab6954ec3d62c1818b8954a802297c3914cbd
RuslanKrasiy <krasiyruslan@gmail.com>
2019-04-22 21:23:30 +0200

    crear footer and header

--- src/index.php
+++ src/index.php
@@ -9,13 +9,154 @@
     <script src="semantic/out/semantic.min.js"></script>
 </head>
 <body>
-
+<header class='ui fluid container blue inverted segment'>
+    <div class='ui two column doubling stackable grid container'>
+        <div class='column'><p>Centro de Estetica</p></div>
+        <div class='column'>
+            <div class='ui rigth aligned'>
+                <p>Segundo</p>
+            </div>
+        </div>
+    </div>
+</header>
+<section class='ui container'>
 <?php
-//phpinfo();
-include "class/bd.php";
-$bd=new dbase();
-$bd->link;
-echo "<h1> Prueba de mi proyecto1</h1>";
+    //phpinfo();
+    include "class/dbase.php";
+    include "vista/form.php";
+    //$bd=new dbase();
+    //$bd->link;
+    $form=registrar();
+    echo $form;
+    if(isset($_POST['regi'])) print_r($_POST);
 ?>
+</section>
+<footer class='ui fluid raised very padded container inverted segment'style='margin-bottom:0;'>
+    <div class='ui three column doubling stackable grid container'>
+        <div class='column'>
+            <h4>Informacion</h4>
+            <div class='ui padded text inverted segment'>
+                <p><a href='#'>Nuestro equipo</a></p>
+                <p><a href='#'>Política de datos</a></p>
+                <p><a href='#'>Política de cookies</a></p>
+            </div>
+        </div>
+        <div class='column'>
+            <h4>Cantacto</h4>
+            <div class='ui padded text inverted segment'>
+                <p><a href='#'>soporte@gmail.com</a></p>
+                <p>av.Blasco Ibañes 128, 1</p>
+                <p>Valencia, 46014</p>
+            </div>
+        </div>
+        <div class='column'>
+            <h4>Columnas</h4>
+            <div class='ui padded text inverted segment'>
+                <p>Columnas</p>
+            </div>
+        </div>
+    </div>
+    <div class='ui divider'></div>
+    <div class='ui fluid center aligned container'>
+        <p>Diseñado por : <a href='#' class=''>Ruslan Krasiy</a></p>
+    </div>
+    
+</footer>
+
+<script>
+    $('.ui.checkbox')
+    .checkbox()
+    ;
+    $('.ui.form')
+        .form({
+            inline : true,
+            on     : 'blur',
+            fields: {
+            first_name: {
+                identifier: 'first-name',
+                rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your name'
+                }
+                ]
+            },
+            last_name: {
+                identifier: 'last-name',
+                rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your name'
+                }
+                ]
+            },
+            email: {
+                identifier: 'email',
+                rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your correct email'
+                },
+                {
+                    type    :   "email",
+                    prompt  :  "MAL"
+                }
+                ]
+            },
+            
+            password: {
+                identifier: 'password',
+                rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter a password'
+                },
+                {
+                    type   : 'minLength[6]',
+                    prompt : 'Your password must be at least {ruleValue} characters'
+                }
+                ]
+            },
+            day: {
+                identifier: 'day',
+                rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your birthday'
+                }
+                ]
+            },
+            month: {
+                identifier: 'month',
+                rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your month'
+                }
+                ]
+            },
+            year: {
+                identifier: 'year',
+                rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your year'
+                }
+                ]
+            },
+            }
+        });
+
+//kk
+if( $('.ui.form').form('is valid')) {
+  // form is valid (both email and name)
+    console.log("TOFO ad");
+}
+
+
+
+
+
+</script>
 </body>
 </html>
--- src/vista/form.php
+++ src/vista/form.php
@@ -1,5 +1,85 @@
 <?php
-echo"<form method='POST' ";
+function registrar(){
+    $form="<div class='ui container'>";
+    $form.="<form method='post' class='ui form' action=''>";
+    $form.="<h4 class='ui dividing header'>Crea una cuenta</h4>";
+    $form.="<div class='required field'>";
+    //$form.="<label>Nombre</label>";
+        $form.="<div class='two fields'>";
+            $form.="<div class='field'>";
+                $form.="<input type='text' name='first-name' 
+                    placeholder='Nombre'></div>";
+            $form.="<div class='field'>";
+                $form.="<input type='text' name='last-name' 
+                    placeholder='Apellido'></div>";
+        $form.="</div>";
+    $form.="</div>";   
+    $form.="<div class='required field'>";
+        $form.="<div class='fields'>";
+            $form.="<div class='twelve wide field'>";
+                $form.="<input type='email' name='email' 
+                    placeholder='Correo electronico'></div>";
+        $form.='</div>';
+        $form.="<div class='required field'>";
+            $form.="<div class='six wide field'>";
+            $form.="<input type='password' name='password'
+            placeholder='Contraseña'></div>";
+        $form.="</div>";
 
+        $form.="<div class='three fields'>";
+            $form.="<label>Fecha de nacimiento</label>";
+            $form.="<div class='two wide required field'>";
+                $form.="<select class='ui search dropdown' name='day'>";
+                $form.="<option value=''>Dia</option>";
+                    for($i=1;$i<32;$i++){
+                        $form.="<option value='$i'>$i</option>";
+                    }
+                    
+                $form.="</select>";
+            $form.="</div>";
+
+            $form.="<div class='two wide required field'>";
+                $form.="<select class='ui search dropdown'  name='month'>";
+                    $form.="<option value=''>Mes</option>";
+                    $form.="<option value='1'>Enero</option>";
+                    $form.="<option value='2'>Febrero</option>";
+                    $form.="<option value='3'>Marzo</option>";
+                    $form.="<option value='4'>Abril</option>";
+                    $form.="<option value='5'>Mayo</option>";
+                    $form.="<option value='6'>Junio</option>";
+                    $form.="<option value='7'>Julio</option>";
+                    $form.="<option value='8'>Agosto</option>";
+                    $form.="<option value='9'>Septiembre</option>";
+                    $form.="<option value='10'>Octubre</option>";
+                    $form.="<option value='11'>Noviembre</option>";
+                    $form.="<option value='12'>Deciembre</option>";
+                $form.="</select>";
+            $form.="</div>";
+
+        $form.="<div class='two wide required field'>";
+                $form.="<select class='ui search dropdown' name='year'>";
+                    $form.="<option value=''>Año</option>";
+                    for($i=2010;$i>1955;$i--){
+                        $form.="<option value='$i'>$i</option>";
+                    }
+                    
+                $form.="</select>";
+            $form.="</div>";
+        
+        $form.="</div>";
+
+        $form.="<div class='inline field'>";
+                $form.="<label>Al hacer clic en Registrar , aceptas nuestras <a href='#'>Condiciones</a>
+                . Obtén más información sobre cómo recopilamos, usamos y compartimos tu información en la 
+                <a href='#'>Política de datos</a>, así como el uso que hacemos de la cookies y tecnologías similares
+                en nuestra <a href='#'>Política de cookies</a>.</label>";
+            $form.="</div>";
+        $form.="</div>";
+        $form.="<input type='submit' class='ui blue submit button' name='regi' value='Registrarse'>";
+        $form.="<div class='ui error message'></div>";
+    $form.="</form>";
+    $form.="</div>";
+    return $form;
+}
 
 ?>
